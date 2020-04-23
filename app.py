38dfceef2e2579d3c30ba127495c7ef864798675
anitha-hub <anitha@danfishel.com>
2020-04-23 23:46:59 +0530

    all REST methods code included

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def updatepost(id):
         return response.text
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
